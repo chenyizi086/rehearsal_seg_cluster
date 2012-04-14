82c9009900e7c8f16dcf8a4f266f1c684cb0fda9
Dawen Liang <cheerz827@gmail.com>
2012-04-14 03:12:51 -0400

    Fixed typos in HMM_Smoother

--- hmm_smoother.cpp
+++ hmm_smoother.cpp
@@ -6,8 +6,9 @@
 #define MAX(x, y) x > y ? x:y
 
 void HMM_Smoother::do_smooth(vector<float> ada_raw_result, vector<int> &result_with_smooth) {
-	vector<float> p_so;		// p(state | observation)
 	vector<float> viterbi_m, viterbi_n;
+	vector<float> p_os_m;		// p(observation | state = music)
+	vector<float> p_os_n;		// p(observation | state = noise)
 	vector<int> path_m, path_n;
 
 	int i, ind, n_lframe = ada_raw_result.size();
